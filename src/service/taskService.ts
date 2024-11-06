9d6654cf4c24c8b47129ece2c80ac11685842cae
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-11-06 02:44:19 -0300

    feat: base task service --in progress

---
+++ src/service/taskService.ts
@@ -0,0 +1,10 @@
+import { prisma } from "../prisma";
+import { TTAsk } from "../types/TTask";
+
+export const createTask = async (data: TTAsk) => {}
+
+export const getTasks = async () => {}
+
+export const updateTask = async (id: string, data: TTAsk) => {}
+
+export const deleteTask = async (id: string) => {}
