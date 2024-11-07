1372d44425c1c80d89a0199496d8f05b146284f6
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-11-06 23:29:04 -0300

    feat: create services task

--- src/service/taskService.ts
+++ src/service/taskService.ts
@@ -1,10 +1,18 @@
 import { prisma } from "../prisma";
 import { TTAsk } from "../types/TTask";
 
-export const createTask = async (data: TTAsk) => {}
+export const createTask = async (data: TTAsk) => {
+  return await prisma.task.create({data})
+}
 
-export const getTasks = async () => {}
+export const getTasks = async () => {
+  return await prisma.task.findMany()
+}
 
-export const updateTask = async (id: string, data: TTAsk) => {}
+export const updateTask = async (id: string, data: TTAsk) => {
+  return await prisma.task.update({where: {id}, data})
+}
 
-export const deleteTask = async (id: string) => {}
+export const deleteTask = async (id: string) => {
+  return await prisma.task.delete({where: {id}})
+}
